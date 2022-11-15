c809359159d3c9fef6d2c87ee536c8e327cf5d3b
kerdai2001 <99104452+kerdai2001@users.noreply.github.com>
2022-11-15 03:02:48 -0500

    Initial Commit

---
+++ server/models/playlist-model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const playlistSchema = new Schema(
+    {
+        name: { type: String, required: true },
+        ownerEmail: { type: String, required: true },
+        songs: { type: [{
+            title: String,
+            artist: String,
+            youTubeId: String
+        }], required: true }
+    },
+    { timestamps: true },
+)
+
+module.exports = mongoose.model('Playlist', playlistSchema)
